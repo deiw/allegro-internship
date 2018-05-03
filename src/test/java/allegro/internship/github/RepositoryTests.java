7bb40f1266f5b63f8b278734b32348494833a95e
deiw <majorczyk.dawid@gmail.com>
2018-05-03 21:04:13 +0200

    Added test of successfully response

--- src/test/java/allegro/internship/github/RepositoryTests.java
+++ src/test/java/allegro/internship/github/RepositoryTests.java
@@ -1,13 +1,22 @@
 package allegro.internship.github;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.OffsetDateTime;
+
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.ArgumentMatchers.startsWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,6 +29,7 @@ public class RepositoryTests {
 
     @Test(expected = GitHubUnavailableError.class)
     public void forNullResponse_shouldThrowAssertionError() {
+        //setup
         when(restTemplate.getForObject(anyString(), eq(Repository[].class))).thenReturn(null);
 
         // execute
@@ -30,11 +40,28 @@ public class RepositoryTests {
 
     @Test(expected = RepositoryNotFoundException.class)
     public void forEmptyRepositories_shouldThrowRepositoryNotFoundException() {
+        //setup
         when(restTemplate.getForObject(anyString(), eq(Repository[].class))).thenReturn(new Repository[]{});
 
         // execute
         Repository lastModifiedRepository = repositoryService.getLastModifiedRepository();
 
         // verify
+    }
+
+    @Test
+    public void afterSuccessfullyResponse_shouldReturnRepository() {
+        //setup
+        when(restTemplate.getForObject(anyString(), eq(Repository[].class))).thenReturn(new Repository[]{
+                new Repository("FirstRepo", OffsetDateTime.now()),
+                new Repository("SecondRepo", OffsetDateTime.now().minusDays(1)),
+                new Repository("ThirdRepo", OffsetDateTime.now().plusHours(1))
+        });
+
+        //execute
+        Repository lastModifiedRepository = repositoryService.getLastModifiedRepository();
+
+        //verify
+        Assert.assertNotNull(lastModifiedRepository);
     }
 }
